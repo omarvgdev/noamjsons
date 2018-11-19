46704f81606da3f96cca5a2e89d688d8f3607581
Omar Ramos Espinosa <omar.vgdev@gmail.com>
2018-11-19 12:56:16 -0600

    change index to json

--- EN/index.js
+++
@@ -1,4 +0,0 @@
-export const version = 1;
-export { default as stylesData } from "./stylesData";
-export { default as placesData } from "./placesData";
-export { default as appData } from "./appData";
